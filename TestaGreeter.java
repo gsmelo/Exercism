8fdeb2b10e11518b97d0ceb554b9fdead4320b19
Giovana de Melo <38022852+gsmelo@users.noreply.github.com>
2021-12-21 17:35:59 -0300

    Delete TestaGreeter.java

--- TestaGreeter.java
+++
@@ -1,9 +0,0 @@
-public class TestaGreeter {
-
-    public static void main(String[] args) {
-
-        Greeter teste = new Greeter();
-        teste.setGreeting("Hello World!");
-        System.out.println(teste.getGreeting());
-    }
-}
